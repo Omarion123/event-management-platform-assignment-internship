58d5718ff224b29fa9a678d4da011a016e28d1f7
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 <anzymen@gmail.com>
2024-04-27 21:58:16 +0200

    fixed user role priveledge on booking view

--- back-end-events/src/router/booking.ts
+++ back-end-events/src/router/booking.ts
@@ -10,8 +10,9 @@ import {
 import { isAuthenticated, isAdmin, isUser } from '../middlewares';
 
 export default (router: express.Router) => {
-  router.get('/bookings', isAdmin, isUser, getAllBookingsController);
   router.get('/bookings/:id', isAdmin, getBookingController);
+  router.get('/bookings', isUser, getAllBookingsController);
+  router.get('/bookingsadmin', isAdmin, getAllBookingsController);
   router.post('/bookings', isAuthenticated, isUser, createBookingController);
   router.post('/bookings/:id', isAuthenticated, isUser, cancelBookingController);
   router.patch('/bookings/:id', isAuthenticated,isAdmin, updateBookingController);
